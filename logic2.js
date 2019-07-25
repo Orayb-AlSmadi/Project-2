4f4b87ea6b89336ebd734960cb9a78f1e8f97be9
orayb-alsmadi <oraybalsmadi@yahoo.com>
2019-07-25 18:28:27 +0300

    reset + next button

--- logic2.js
+++ logic2.js
@@ -3,17 +3,50 @@ var clic = 1;
 var n = [];
 var z = [true, true, true, true, true, true, true, true, true]
 var names = ["player 1", "player 2"]
-var xwin =0; 
-var owin =0;
+var xwin = 0;
+var owin = 0;
 
-$("#play").click(function (){
-  
-   $("#page1").css("display", "none");
-//    $("#page2").css("display", "block");
-    $("#page2").fadeIn( 1000 );
-    if($("#p1n").val() !== "")
+$("#next").click(function () {
+
+    clic = 1;
+    var n = [];
+     z = [true, true, true, true, true, true, true, true, true];
+    $("#turn").text("player 1")
+    $("#n0, #n1, #n2, #n3 , #n4, #n5, #n6, #n7, #n8").css("color", "white")
+    for (var i=0; i<9; i++ ) {
+    document.getElementById("n" +i).innerHTML = "";
+}
+}
+)
+
+$("#reset").click(function () {
+
+    clic = 1;
+    var n = [];
+     z = [true, true, true, true, true, true, true, true, true];
+    $("#turn").text("player 1")
+    $("#n0, #n1, #n2, #n3 , #n4, #n5, #n6, #n7, #n8").css("color", "white")
+    for (var i=0; i<9; i++ ) {
+    document.getElementById("n" +i).innerHTML = "";
+    }
+     xwin = 0;
+     owin = 0;
+     $("#xwin").text(xwin)
+ $("#owin").text(owin)
+}
+)
+function reloadPage(){
+    location.reload(true);
+}
+
+$("#play").click(function () {
+
+    $("#page1").css("display", "none");
+    //    $("#page2").css("display", "block");
+    $("#page2").fadeIn(1000);
+    if ($("#p1n").val() !== "")
         names[0] = $("#p1n").val();
-    if($("#p2n").val() !== "")
+    if ($("#p2n").val() !== "")
         names[1] = $("#p2n").val();
 
     $("#name1").text(names[0]);
@@ -22,27 +55,27 @@ $("#play").click(function (){
 }
 )
 
-$("#main").click(function (){
+$("#main").click(function () {
     if (z.indexOf(true) == -1) {
         return
     }
     if (clic % 2 === 0) {
         $("#turn").text("player 2");
     }
-    else 
-    $("#turn").text("player 1");
+    else
+        $("#turn").text("player 1");
 
 })
 
-function clickchange (container) {
-    debugger
+function clickchange(container) {
+    
     if (z[container.currentTarget.id] === true) {
         if (clic % 2 === 0) {
-            document.getElementById("n"+container.currentTarget.id).innerHTML = "O"
+            document.getElementById("n" + container.currentTarget.id).innerHTML = "O"
             clic++;
         }
         else {
-            document.getElementById("n"+container.currentTarget.id).innerHTML = "X"
+            document.getElementById("n" + container.currentTarget.id).innerHTML = "X"
             clic++;
         }
         z[container.currentTarget.id] = false;
@@ -50,14 +83,14 @@ function clickchange (container) {
     check();
 }
 
-for(var i = 0; i < 9; i++) {
+for (var i = 0; i < 9; i++) {
     var divelement = document.createElement('div');
     divelement.id = i;
     var parg = document.createElement('p');
-    var pargId = "n"+ i
+    var pargId = "n" + i
     parg.id = pargId;
     divelement.appendChild(parg);
-    divelement.onclick = clickchange 
+    divelement.onclick = clickchange
     document.getElementById('main').appendChild(divelement);
 }
 
@@ -68,95 +101,129 @@ function check() {
         n[i] = $(s).text()
     }
 
+    // var q= z.indexOf(true) - 1
+    debugger
+    if (( z.indexOf(true) - 1 != -2 )) {
+        if (n[0] === n[1] && n[0] === n[2] && n[0] !== "") {
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[0] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
 
-if ((z.indexOf(true) !== -1)) {
-    if (n[0] === n[1] && n[0] === n[2]
-        && n[0] !== "") {
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[0] === "X"){
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
-            $("#xwin").text(xwin)
-            
             }
-        else {
-             $("#turn").text("o winner")
-             owin++;
-             $("#owin").text(owin)
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
+            }
+            $("#n0, #n1, #n2").css("color", "yellow")
         }
-        $("#n0, #n1, #n2").css("color", "yellow")
-    }
-    if (n[3] === n[4] && n[4] === n[5] && n[3] !== "") {
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[3] === "X")
-        {
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
-            }
-        else
-             $("#turn").text("o winner")
-    }
-    if (n[6] === n[7] && n[6] === n[8] && n[6] !== "") {
+        if (n[3] === n[4] && n[4] === n[5] && n[3] !== "") {
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[3] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
 
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[6] === "X")
-        {
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
             }
-        else
-             $("#turn").text("o winner")
-    }
-    if (n[0] === n[3] && n[0] === n[6] && n[0] !== "") {
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[0] === "X")
-        {
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
             }
-        else
-             $("#turn").text("o winner")
-    }
-    if (n[1] === n[4] && n[1] === n[7] && n[1] !== "") {
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[4] === "X")
-        {
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
-            }
-        else
-             $("#turn").text("o winner")
-    }
-    if (n[2] === n[5] && n[2] === n[8] && n[2] !== "") {
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[2] === "X")
-        {
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
-            }
-        else
-             $("#turn").text("o winner")
+            $("#n3, #n4, #n5").css("color", "yellow")
+        }
+        if (n[6] === n[7] && n[6] === n[8] && n[6] !== "") {
 
-    }
-    if (n[2] === n[4] && n[2] === n[6] && n[2] !== "") {
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[2] === "X")
-        {
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[6] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
+
             }
-        else
-             $("#turn").text("o winner")
-    }
-    if (n[0] === n[4] && n[0] === n[8] && n[0] !== "") {
-        z = [false, false, false, false, false, false, false, false, false]
-        if (n[0] === "X")
-        {
-             $("#turn").text(names[0]+" is the winner");
-            xwin++;
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
             }
-        else
-             $("#turn").text("o winner")
+            $("#n6, #n7, #n8").css("color", "yellow")
+        }
+        if (n[0] === n[3] && n[0] === n[6] && n[0] !== "") {
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[0] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
+
+            }
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
+            }
+            $("#n0, #n3, #n6").css("color", "yellow")
+        }
+        if (n[1] === n[4] && n[1] === n[7] && n[1] !== "") {
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[4] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
+            }
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
+            }
+            $("#n4, #n1, #n7").css("color", "yellow")
+        }
+        if (n[2] === n[5] && n[2] === n[8] && n[2] !== "") {
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[2] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
+
+            }
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
+            }
+            $("#n5, #n8, #n2").css("color", "yellow")
+
+        }
+        if (n[2] === n[4] && n[2] === n[6] && n[2] !== "") {
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[2] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
+
+            }
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
+            }
+            $("#n4, #n6, #n2").css("color", "yellow")
+        }
+        if (n[0] === n[4] && n[0] === n[8] && n[0] !== "") {
+            z = [false, false, false, false, false, false, false, false, false]
+            if (n[0] === "X") {
+                $("#turn").text(names[0] + " is the winner");
+                xwin++;
+                $("#xwin").text(xwin)
+
+            }
+            else {
+                $("#turn").text(names[1] + " is the winner")
+                owin++;
+                $("#owin").text(owin)
+            }
+            $("#n0, #n4, #n8").css("color", "yellow")
+        }
     }
-}
 }
