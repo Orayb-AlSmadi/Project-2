7fe9c4f82a28666f0c0d2141dec2b0094d3af7b1
Orayb-AlSmadi <oraybalsmadi@yahoo.com>
2019-07-28 11:07:06 +0300

    Final version of Orayb & Deyaa project

--- logic.js
+++ logic.js
@@ -1,39 +1,39 @@
 $(document).ready();
-var clic = 1;
+var clic = 1; // initializing the click counter to 1
 var n = [];
-var z = [true, true, true, true, true, true, true, true, true];
-var names = ["player 1", "player 2"];
-var xwin = 0;
-var owin = 0;
-var gameFinish = false;
-var u = 0;
+var z = [true, true, true, true, true, true, true, true, true]; // initializing the x-o divs to be pressable
+var names = ["player 1", "player 2"]; // initializing the players name defaults 
+var xwin = 0; // initializing x wins counter to 0
+var owin = 0;// initializing o wins counter to 0
+var gameFinish = false; // initializing the variable that indicates if the games finsihed or not
+var u = 0; // this is a variable indacates if its mutliplayer or single player ( single player, u =1), (multiplayer, u = 0)
 
 $("#next").click(function() {
-  clic = 1;
+  clic = 1; //reinitializing click counter to 1
   var n = [];
-  z = [true, true, true, true, true, true, true, true, true];
-  $("#turn").text(names[0]);
-  $("#n0, #n1, #n2, #n3 , #n4, #n5, #n6, #n7, #n8").css("color", "white");
+  z = [true, true, true, true, true, true, true, true, true]; //reinitializing the x-o divs to be pressable
+  $("#turn").text(names[0]); // reinitializing the turn name to player 1's name
+  $("#n0, #n1, #n2, #n3 , #n4, #n5, #n6, #n7, #n8").css("color", "white"); // reinitializing the css of x-o divs to default 
   for (var i = 0; i < 9; i++) {
-    document.getElementById("n" + i).innerHTML = "";
+    document.getElementById("n" + i).innerHTML = ""; // reinitializing the divs content to empty
   }
-  if (u === 1) {
-    //(CHANGE ON CODE) to reset the computer variables and to put x on the middle
-    clic = 0;//(CHANGE ON CODE) to prevent the player to put O before the computer put x on mid
-    setTimeout(xOnMid,700); //(CHANGE ON CODE) time out for putting x on mid (to make it more realistic)
-    z[4] = false;
-    scenario1 = 0;
+  if (u === 1) { //to reset the computer variables and to put x on the middle
+    
+    clic = 0;//to prevent the player to put O before the computer put x on mid
+    setTimeout(xOnMid,700); // time out for putting x on mid (to make it more realistic)
+    z[4] = false; // make the middle div of x-o unpressable
+    scenario1 = 0; // to make all scenarioes 0 to recheck which scenario to enter
     scenario2 = 0;
     scenario3 = 0;
     scenario4 = 0;
     scenario5 = 0;
     
   }
-  gameFinish = false;
+  gameFinish = false; // initializing the variable that indicates if the games finsihed or not
 });
 
-$("#reset").click(function() {
-  clic = 1;
+$("#reset").click(function() { // another way to reset (we actually used the reload function)
+  clic = 1; 
   var n = [];
   z = [true, true, true, true, true, true, true, true, true];
   $("#turn").text("player 1");
@@ -47,36 +47,36 @@ $("#reset").click(function() {
   $("#owin").text(owin);
 });
 
-function reloadPage() {
+function reloadPage() { // this function to reload the page
   location.reload(true);
 }
 
 $("#singleplayer").click(function() {
-  u = 1;
-  clic = 0; //(CHANGE ON CODE) to prevent the player to put O before the computer put x on mid
-  $("#page1").css("display", "none");
-  $("#page2").fadeIn(1000);
-  if ($("#singlePlayerName").val() !== "") names[1] = $("#singlePlayerName").val();
-  names[0] = "Computer";
-  $("#name1").text(names[0]);
+  u = 1; // make u = 1 to let the computer use the comuter algorithim function to play with
+  clic = 0; //to prevent the player to put O before the computer put x on mid
+  $("#page1").css("display", "none"); // to hide the introduction page
+  $("#page2").fadeIn(1000); // to display the game page in a specific duration
+  if ($("#singlePlayerName").val() !== "") names[1] = $("#singlePlayerName").val(); // to take the players name if he input it else he take the default name (player)
+  names[0] = "Computer"; // take the computer name
+  $("#name1").text(names[0]); // to display the player 1 name (computer)
 
-  $("#name2").text(names[1]);
-  $("#turn").text(names[0]);
-  setTimeout(xOnMid,700); //(CHANGE ON CODE) time out for putting x on mid (to make it more realistic)
+  $("#name2").text(names[1]); // to display the player 2 name
+  $("#turn").text(names[0]); // to display the player name in turn box
+  setTimeout(xOnMid,700); // time out for putting x on mid (to make it more realistic)
 //   $("#turn").text(names[1]);//(CHANGE ON CODE) delete this statement; we added it in function xOnMid, to let the turn changes to player's turn after the computer put x on the middle
-  z[4] = false;
+  z[4] = false; // to make the middle div in x-o unpressable
 });
 
-function xOnMid () //(CHANGE ON CODE) function to put x on the middle
+function xOnMid () // function to put x on the middle
 {
-    $("#n4").text("X");
-    $("#turn").text(names[1]);
-    clic++; //(CHANGE ON CODE) after the computer put x on mid, the player now can put O
+    $("#n4").text("X"); // to put x on the middle as the first move for the computer
+    $("#turn").text(names[1]); // to change the turn for the player 2 (which is the human)
+    clic++; // after the computer put x on mid, the player now can put O
 }
 
-$("#multiplayer").click(function() {
+$("#multiplayer").click(function() { //same process as the singleplayer's process
   u = 0;
-  $("#page1").css("display", "none");
+  $("#page1").css("display", "none"); 
   $("#page2").fadeIn(1000);
   if ($("#p1n").val() !== "") names[0] = $("#p1n").val();
   if ($("#p2n").val() !== "") names[1] = $("#p2n").val();
@@ -86,7 +86,7 @@ $("#multiplayer").click(function() {
   $("#turn").text(names[0]);
 });
 
-$("#main").click(function() {
+$("#main").click(function() { // this function to change the players turn depending on the clic value
   if (z.indexOf(true) == -1) {
     return;
   }
@@ -99,7 +99,7 @@ $("#main").click(function() {
   }
 });
 
-function clickchange(container) {
+function clickchange(container) { // this function to put x or o in the x-o divs and changing the clic value (and checking before if the div can be pressable or not)
   if (u === 0) {
     if (z[container.currentTarget.id] === true) {
       if (clic % 2 === 0) {
@@ -109,18 +109,18 @@ function clickchange(container) {
         document.getElementById("n" + container.currentTarget.id).innerHTML = "X";
         clic++;
       }
-      z[container.currentTarget.id] = false;
+      z[container.currentTarget.id] = false; // after he chise a div .. it make the value to false .. so it will be unpressable
     }
-    check();
+    check(); // implement the function (check) to check if anyone wins. depending on if its single player or mutiplayer
   } else if (z[container.currentTarget.id] === true) {
     if (clic % 2 !== 0) {
       document.getElementById("n" + container.currentTarget.id).innerHTML = "O";
-      check(); //(CHANGE ON CODE) to check if the player wins before the computer do the next move
-      clic = clic + 1; //(CHANGE ON CODE) changed from clic +2 to clic + 1 and the other +1 is inside the computer function .. and like this the player cant click untill the computer function ends.
+      check(); //to check if the player wins before the computer do the next move
+      clic = clic + 1; //change clic value by adding 1 (clic + 1) and the other +1 is inside the computer function .. and like this the player cant click untill the computer function ends.
     //   setTimeout(clicplus, 1000);
-      if(gameFinish === false) // //(CHANGE ON CODE) to prevent the computer to put x if the game finish
+      if(gameFinish === false) // to prevent the computer to put x if the game finish
       setTimeout(computer, 1000);
-      z[container.currentTarget.id] = false; //(CHANGE ON CODE) moved this inside the if to fix the bug
+      z[container.currentTarget.id] = false; //(CHANGE ON CODE) moved this inside the if to fix the bug to make the div unclickable
     }
     
   }
@@ -130,12 +130,12 @@ function clickchange(container) {
 //     clic = clic + 1;
 // }
 
-var scenario1 = 0; //(CHANGE ON CODE) this variable is for computer turns number
+var scenario1 = 0; //this variable is for computer turns number based on the scenario
 var scenario2 = 0;
 var scenario3 = 0;
 var scenario4 = 0;
 var scenario5 = 0;
-function computer() {
+function computer() { // this function is the algorithim to play vs computer (single player)
   for (var i = 0; i < 9; i++) {
     var s = "#n" + i;
     n[i] = $(s).text();
@@ -317,7 +317,7 @@ function computer() {
   clic = clic + 1; //(CHANGE ON CODE) we discussed why we add this statetemnt above
 }
 
-for (var i = 0; i < 9; i++) {
+for (var i = 0; i < 9; i++) { // to make the x-o divs instead of making this in the html file
   var divelement = document.createElement("div");
   divelement.id = i;
   var parg = document.createElement("p");
@@ -328,17 +328,17 @@ for (var i = 0; i < 9; i++) {
   document.getElementById("main").appendChild(divelement);
 }
 
-function finalwin() {
+function finalwin() { //this function to show and display the final winner
   $("#page2").css("display", "none");
   $("#page3").fadeIn(1500);
   if (xwin === 2) {
-    $("#winner").text(names[0] + " is the final winner");
+    $("#winner").text("Congratulations "+ names[0] + " you are the WINNER");
   } else if (owin === 2) {
-    $("#winner").text(names[1] + " is the final winner");
+    $("#winner").text("Congratulations "+ names[1] + " you are the WINNER");
   }
 }
 
-function check() {
+function check() { // this funtion to check who wins and display the winner on turn box and change the line of o or x to y that wins to yellow
   if (gameFinish) {
     return;
   }
